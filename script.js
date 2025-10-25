904aa3f5e4ff7b5c2688d9636e5c09b040de6b44
Colby-williams <wil21128@byui.edu>
2025-10-25 02:48:00 -0600

    feat: update navigation links and enhance header functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+function toggleSidebar(button) {
+    button.classList.toggle("change");
+    document.querySelector("header").classList.toggle("collapsed");
 }
 document.getElementById("myButton").addEventListener("click", function() {
     myFunction(this);

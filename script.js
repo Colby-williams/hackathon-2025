bca83968cf0be7cd16a55ee624b1bfc709ac9a21
Colby-williams <wil21128@byui.edu>
2025-10-25 03:44:24 -0600

    fix: correct function call in index.html and update styles for collapsed header

--- script.js
+++ script.js
@@ -2,6 +2,3 @@ function toggleSidebar(button) {
     button.classList.toggle("change");
     document.querySelector("header").classList.toggle("collapsed");
 }
-document.getElementById("myButton").addEventListener("click", function() {
-    myFunction(this);
-});
